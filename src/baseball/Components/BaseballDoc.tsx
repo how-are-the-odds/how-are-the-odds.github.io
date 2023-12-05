8ef3d00aa2ca1e4e2531c03a1e033bc1a73d4ea5
Daniel-Packer <packer.61@osu.edu>
2023-12-05 15:03:31 -0500

    add Baseball model explainer

--- src/baseball/Components/BaseballDoc.tsx
+++ src/baseball/Components/BaseballDoc.tsx
@@ -1,4 +1,18 @@
-const BaseballDoc = () => {return <p>This is how the model works</p>};
+const BaseballDoc = () => {
+  return (
+    <>
+      <p>
+        The model finds vectors (in this case in two dimensions) to represent
+        each pitcher and batter for each outcome, and additionally computes an
+        offset for each count. To produce a prediction, we select the vectors
+        for the batter and the pitcher and compute the pairwise inner products
+        over the outcomes. These "scores" are then combined with the
+        county-offset to produce a score for each possible outcome. Finally, a
+        simple softmax function is applied to convert the output to a
+        probability distribution.
+      </p>
+    </>
+  );
+};
 
-
-export default BaseballDoc
+export default BaseballDoc;
