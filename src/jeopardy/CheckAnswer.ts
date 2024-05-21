38b39f230337dd89ecb067bfbf54cbe640cc2cec
Daniel-Packer <packer.61@osu.edu>
2024-05-20 22:11:22 -0400

    more flexible answer checking

--- src/jeopardy/CheckAnswer.ts
+++ src/jeopardy/CheckAnswer.ts
@@ -2,11 +2,20 @@ const hyperSimplify = (str: string) => {
   return str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
 };
 
+const removeArticles = (str: string) => {
+    return str.replace(/^(a|an|the)\s+/i, "");
+    };
+
+const removeParenthetical = (str: string) => {
+    return str.replace(/\(.*\)/, "");
+}
+
 export const checkAnswer: (
   answer: string,
   response: string
 ) => "correct" | "maybe" | "incorrect" = (answer: string, response: string) => {
-  if (hyperSimplify(answer) === hyperSimplify(response)) {
+  const responses = [response, removeArticles(response), removeParenthetical(response), removeArticles(removeParenthetical(response))].map(hyperSimplify);
+  if (responses.includes(removeArticles(hyperSimplify(answer)))) {
     return "correct";
   }
 
