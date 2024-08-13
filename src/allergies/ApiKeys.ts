e8ebd9e6b59999c2ec5688b6a15a90e62abf96bb
Daniel Packer <62404584+Daniel-Packer@users.noreply.github.com>
2024-08-13 18:12:47 -0400

    Update ApiKeys.ts

--- src/allergies/ApiKeys.ts
+++ src/allergies/ApiKeys.ts
@@ -1,2 +1,3 @@
+// Free API Keys, still them if you want, I guess...
 export const SPOONACULAR_API_KEY = "e825ca4a435f4cb8a3c19cc54100ce21";
 export const FDA_API_KEY = "TFECoIJ1L4a4Lkl0RHt0eo7vkTkJAIij6nVAbNx0"
